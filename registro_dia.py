1eb988a38caeae89dc2327b529ac8d260930f0d8
Gabriel Vilela Zamberlan <Gabriel Vilela Zamberlan>
2025-05-13 16:54:25 -0300

    incluindo o banco de dados

--- registro_dia.py
+++ registro_dia.py
@@ -2,8 +2,21 @@ import customtkinter as ctk
 import tkinter as tk
 from tkcalendar import Calendar
 import subprocess
+import sqlite3
 
+conexao= sqlite3.connect('registro_clientes.db')
+cursor=conexao.cursor()
 
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS eventos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        serviço TEXT NOT NULL
+        data TEXT NOT NULL
+        horario TEXT NOT NULL
+    )
+''')
+
+conexao.commit()
 
 ctk.set_appearance_mode("white")
 ctk.set_default_color_theme("blue")
@@ -13,13 +26,19 @@ jan.title()
 jan.configure(fg_color="#DEB887")
 
 def ok():
+    data=calendario.get_date()
+    if nome.strip and emailok.strip():
+        cursor.execute('INSERT INTO usuarios (data,) VALUES (?)', (data,))
+        conexao.commit()
+        print(f"Data {data} salvo com sucesso")
+
     subprocess.Popen(["python","registro_serviço.py"])
     jan.iconify()
     jan.destroy()
 
 
 
-calendario= Calendar(jan,selectmode='day',font=("Arial",15)) 
+calendario= Calendar(jan,date_pattern='dd/mm/yy',font=("Arial",15)) 
 calendario.pack(pady=20)
 
 confirm= ctk.CTkButton(jan,text="confirmar",font=("Arial",20),fg_color="#006400",command=ok)
--- registro_nome.py
+++ registro_nome.py
@@ -1,8 +1,17 @@
 import customtkinter as ctk
 import subprocess
+import sqlite3
 
+conexao= sqlite3.connect('registro_clientes.db')
+cursor=conexao.cursor()
 
-
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS usuarios(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL
+)
+''')
 
 ctk.set_appearance_mode("white")
 ctk.set_default_color_theme("blue")
@@ -12,6 +21,13 @@ jan.title()
 jan.configure(fg_color="#DEB887")
 
 def mudar ():
+    nome=nom.get()
+    emailok=email.get()
+    if nome.strip and emailok.strip():
+        cursor.execute('INSERT INTO usuarios (nome,email) VALUES (?,?)', (nome,emailok))
+        conexao.commit()
+        print(f"Nome {nome} e Email {email} salvo com sucesso")
+
     subprocess.Popen(["python","registro_dia.py"])
     jan.iconify()
     jan.destroy()
@@ -19,14 +35,23 @@ def mudar ():
 
 ok=ctk.CTkLabel(jan, text="Nome:", font=("Arial",25))
 ok.pack(pady=15,anchor="w",padx=20)
+
 nom=ctk.CTkEntry(jan,height=15,width=300,font=("Arial",20))
 nom.pack(padx=10,anchor="w",pady=1)
+
 okok=ctk.CTkLabel(jan, text="Email:", font=("Arial",25))
 okok.pack(pady=15,anchor="w",padx=20)
+
 email=ctk.CTkEntry(jan,height=15,width=300,font=("Arial",20))
 email.pack(padx=10,anchor="w",pady=1)
 
 confirm=ctk.CTkButton(jan,text="confirmar", font=("Arial",30),width=200,height=65,fg_color="#006400",command=mudar)
 confirm.pack(pady=40)
 
+
+
+
+
 jan.mainloop()
+
+conexao.close()
